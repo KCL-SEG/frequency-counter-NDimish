a1028753b5132ff3cfa5c00ca52e4611cf8f8fe1
lazyN <62480903+NDimish@users.noreply.github.com>
2022-09-30 11:38:34 +0100

    typcasting

--- frequencies.py
+++ frequencies.py
@@ -1,13 +1,14 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-
 def frequencies(items):
     frequencies = {}
     for x in items:
+        x = str(x)
         if x in frequencies.keys():
             temp = frequencies[x]
             frequencies[x] = temp+1;
         else:
             frequencies[x] = 1
     return frequencies
+
